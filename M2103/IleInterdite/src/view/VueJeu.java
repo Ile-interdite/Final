4c621c91b1c876b8d33c266e6810d7688918e639
Voltariuss <loic.duboistermoz@gmail.com>
2018-06-21 02:24:28 +0200

    Maj

--- M2103/IleInterdite/src/view/VueJeu.java
+++ M2103/IleInterdite/src/view/VueJeu.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
